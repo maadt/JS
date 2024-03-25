5a106bc78a31d2a79eb7fb8ce37418b1ae63bcff
maadt <147260993+maadt@users.noreply.github.com>
2024-03-26 01:04:40 +0900

    課題２実装完了

--- basic.js
+++ basic.js
@@ -11,3 +11,15 @@ console.log(str2);
 console.log(num1);
 console.log(num2);
 console.log(flg);
+
+//課題２
+
+let numbers = [1, 4, 7, 9];
+let members = ['太郎', '花子'];
+
+for (let i = 0; i < numbers.length;) {
+    console.log(numbers[i]);
+}
+for (let i = 0; i < members.length;) {
+    console.log(members[i]);
+}
