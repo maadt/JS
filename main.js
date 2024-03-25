f7083b923397411d47c9be88d695dd222804c3e9
maadt <147260993+maadt@users.noreply.github.com>
2024-03-26 00:40:20 +0900

    JavaScript/Chapter01/Lesson02
    FirstCommit

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let header = document.querySelector("h1");
+    header.textContent = "Hello world!";
+});
+/*
+let header = document.querySelector("h1");
+//let：変数の宣言
+//document：JavaScriptでウェブページの文書構造を操作するためのオブジェクト。
+//querySelector()：引数に指定されたCSSセレクターに一致する最初の要素を取得する。
+header.textContent = "Hello world!";
+//textContent：指定したテキスト内容を変更。この場合はheaderに格納されたh1タグ。
+*/
