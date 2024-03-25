fd122254543946050594cecd7f4b27809f9b96a8
maadt <147260993+maadt@users.noreply.github.com>
2024-03-26 00:54:06 +0900

    課題１実装完了

---
+++ basic.js
@@ -0,0 +1,13 @@
+//課題１
+
+let str1 = 'abc';
+let str2 = 'defgh';
+let num1 = 300;
+let num2 = 3.14;
+let flg = true;
+
+console.log(str1);
+console.log(str2);
+console.log(num1);
+console.log(num2);
+console.log(flg);
