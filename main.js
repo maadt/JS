5548a12c05a52b29fdc7a6e76282fee03d516384
maadt <147260993+maadt@users.noreply.github.com>
2024-03-30 18:39:49 +0900

    Lesson02

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function () {
     let header = document.querySelector("h1");
     header.textContent = "Hello world!";
@@ -10,3 +11,17 @@ let header = document.querySelector("h1");
 header.textContent = "Hello world!";
 //textContent：指定したテキスト内容を変更。この場合はheaderに格納されたh1タグ。
 */
+function keisan(num) {
+    // 引数 * 3をculcに代入する
+    let culc = num * 3;
+    // culcが9の倍数だったら1を加算する
+    if (culc % 9 === 0) {
+        culc += 1;
+    }
+    return culc;
+}
+
+let num = 2;
+for (let i = 0; i < 10; i++) {
+    num = keisan(num);
+}
